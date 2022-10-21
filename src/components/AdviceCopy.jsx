93527358e64341d28dcb797cd8274676926c72d5
kkulek <106318909+kkulek@users.noreply.github.com>
2022-10-21 13:56:24 +0200

    excluded styling component from functional component

--- src/components/AdviceCopy.jsx
+++ src/components/AdviceCopy.jsx
@@ -1,27 +1,18 @@
-import React from "react";
 import styled from "styled-components";
 import {colorPalette} from "../colorPallete";
 
-export function AdviceCopy({children}) {
-    const AdviceCopy = styled.p`
-      color: ${colorPalette.veryLight};
-      text-align: center;
-      position: relative;
-      font-size: 28px;
-      font-weight: 800;
-      font-family: "Manrope", sans-serif;
+export const AdviceCopy = styled.p`
+  color: ${colorPalette.veryLight};
+  text-align: center;
+  position: relative;
+  font-size: 28px;
+  font-weight: 800;
+  font-family: "Manrope", sans-serif;
 
-      &:before {
-        content: url("../img/pattern-divider-mobile.svg");
-        position: absolute;
-        top: 80px;
-        left: 0;
-      }
-    `
-
-    return (
-        <AdviceCopy>
-            {children}
-        </AdviceCopy>
-    )
-}
+  &:before {
+    content: url("../img/pattern-divider-mobile.svg");
+    position: absolute;
+    top: 80px;
+    left: 0;
+  }
+`
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,23 +2,15 @@ import React from "react";
 import styled from "styled-components";
 import {colorPalette} from "../colorPallete";
 
-export function Card({children}) {
-    const Card = styled.div`
-      background-color: ${colorPalette.dark};
-      width: 90%;
-      min-height: 315px;
-      margin: auto;
-      border-radius: 10px;
-      position: relative;
-      padding: 40px 20px 80px;
-      box-sizing: border-box;
-      max-width: 540px;
-      box-shadow: 30px 50px 80px 5px ${colorPalette.cardShadow};
-    `
-
-    return (
-        <Card>
-            {children}
-        </Card>
-    )
-}
+export const Card = styled.div`
+  background-color: ${colorPalette.dark};
+  width: 90%;
+  min-height: 315px;
+  margin: auto;
+  border-radius: 10px;
+  position: relative;
+  padding: 40px 20px 80px;
+  box-sizing: border-box;
+  max-width: 540px;
+  box-shadow: 30px 50px 80px 5px ${colorPalette.cardShadow};
+`
